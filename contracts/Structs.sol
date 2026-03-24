38bac5eddfc0973a6dbc1e00e45b88a162a5ea23
CallejaJ <posicionadoenlaweb@gmail.com>
2026-03-24 17:58:21 +0100

    feat: structs

--- contracts/Structs.sol
+++ contracts/Structs.sol
@@ -3,25 +3,29 @@ pragma solidity 0.8.18; // solidity version
 
 contract SimpleStorage  {
 
-// favoriteNumber gets initialized to 0 if no value
+uint256 myFavoriteNumber;
+
+// uint256[] listOfFavoriteNumbers; 
+// [77, 78, 90]
 
 // uint256 myFavoriteNumber; // 0
 
-// struct Person {
-//     uint256 favoriteNumber;
-//     string name;
-// }
+struct Person {
+    uint256 favoriteNumber;
+    string name;
+}
 
-uint256 public favoriteNumber; 
+// uint256 public favoriteNumber; 
 
+Person public pat = Person({favoriteNumber: 7, name: "Pat"});
 
 function store(uint256 _favoriteNumber) public {
-    favoriteNumber = _favoriteNumber;
+    myFavoriteNumber = _favoriteNumber;
     // retrieve();
 }
 
 function retrieve() public view returns(uint256) {
-    return favoriteNumber;
+    return myFavoriteNumber;
 }
 
 }
